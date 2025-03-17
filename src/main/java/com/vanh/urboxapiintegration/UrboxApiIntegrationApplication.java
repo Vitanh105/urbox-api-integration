479ef6060e31341fcdded94c79587d71fc3f6fc7
Vitanh105 <vitanh1521@gmail.com>
2025-03-17 13:39:47 +0700

    Initial commit

---
+++ src/main/java/com/vanh/urboxapiintegration/UrboxApiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.vanh.urboxapiintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrboxApiIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrboxApiIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vanh/urboxapiintegration/UrboxApiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanh.urboxapiintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrboxApiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
